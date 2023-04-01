a8aada5790edae71014bf5452c423ed9f701e869
Devoranima <ali3v5t1limAw@gmail.com>
2023-04-01 17:23:46 +0300

    minor design changes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import { query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc } from
 
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  bg: `min-h-screen h-full w-screen max-w-full flex bg-gradient-to-r from-[#75b2ff] to-[#ff7575]`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   form: `flex justify-between`,
   input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-600 text-slate-100`,
+  button: `border p-4 ml-2 bg-[#ff7575] text-white`,
   count: `text-center p-2`,
 }
 
@@ -22,13 +22,18 @@ function App() {
 
   const [todos, setTodos] = useState([])
   const [input, setInput] = useState('')
+  const [wrongInput, setWrongInput] = useState(false)
 
 
   //create todo
   const createTodo = async (e) => {
     e.preventDefault();
     if (input === '') {
-      alert("Enter a valid value for a todo text")
+      //alert("Enter a valid value for a todo text")
+      setWrongInput(true);
+      setTimeout(() => {
+        setWrongInput(false);
+      }, 400);
       return;
     }
 
@@ -79,7 +84,7 @@ function App() {
           className={style.form}
         >
           <input 
-            className={style.input} 
+            className={style.input + (wrongInput ? " invalid" : "")} 
             type="text" 
             placeholder="Add ToDo"
             value={input}
